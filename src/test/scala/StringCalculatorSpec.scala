f70bc596bd1f0123f7fa280953176197ddc40f50
Douglas McCallum <douglas.mccallum@digital.hmrc.gov.uk>
2018-02-21 13:30:07 +0000

    initial commit

---
+++ src/test/scala/StringCalculatorSpec.scala
@@ -0,0 +1,12 @@
+import org.scalatest.{MustMatchers, WordSpec}
+
+class StringCalculatorSpec extends WordSpec with MustMatchers {
+
+  "String Calculator" must {
+
+    "return Int of '0' when provided with an empty string" in {
+      
+    }
+  }
+
+}
