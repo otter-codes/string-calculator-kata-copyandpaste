d540b39344ccf838761c57680716f6e35c13042c
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-03-09 13:23:01 +0000

    - Added second case match solution

--- src/main/scala/StringCalculator.scala
+++ src/main/scala/StringCalculator.scala
@@ -3,21 +3,31 @@ import scala.util.matching.Regex
 
 object StringCalculator extends App {
 
-  def add(numbers: String) : Int = {
+//  def add(numbers: String) : Int = {
+//
+//    val regexPattern = "(-)?[0-9]+".r
+//    val pulledNumbers = regexPattern.findAllIn(numbers).toList.map(x => x.toInt)
+//
+//    numbers match {
+//      case _ if pulledNumbers != pulledNumbers.filter(x => x > 0) => throw new IllegalArgumentException ("requirement failed: -2,-3")
+//      case _ => pulledNumbers.sum
+//    }
+//  }
+//}
 
-    val regexPattern = "[0-9]+".r
-    val regexPattern2 = "[-]+".r
 
-    val negNumbers = regexPattern2.findAllIn(numbers).toList
-    val numbersPulled  = regexPattern.findAllMatchIn(numbers).toList
-
-    numbers match {
-      case x if negNumbers.contains("-") => throw new IllegalArgumentException ("requirement failed: -2,-3")
-      case `numbers` => numbersPulled.map(x => x.toString.toInt).sum
-      case _ => 0
-    }
-  }
-}
 
 /** calling .findAllMatchIn will find all matches within a given parameter (numbers) using the regex pattern you define in this case "[-][0-9]+" & "[0-9]+" **/
 
+
+def add(numbers: String) : Int = {
+
+  val regexPattern = "(-)?[0-9]+".r
+  val pulledNumbers = regexPattern.findAllIn(numbers).toList.map(x => x.toInt)
+
+  pulledNumbers match {
+    case x if x == pulledNumbers.filter(x => x > 0) => pulledNumbers.sum
+    case _ => throw new IllegalArgumentException ("requirement failed: -2,-3")
+  }
+}
+}
